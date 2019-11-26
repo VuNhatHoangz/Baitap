1e93bc28874765f5acd499171e18d032afc9cd5b
VuNhatHoang <55481893+VuNhatHoang@users.noreply.github.com>
2019-11-26 20:57:00 +0700

    first

---
+++ menu.php
@@ -0,0 +1,7 @@
+    <li><a href="http://localhost:91/vuhoangnhat/banghocsinh.php" class="active">Học Sinh</a></li><br/>
+    <li><a href="http://localhost:91/vuhoangnhat/bangdiem.php" class="active">Điểm</a></li><br/>
+    <li><a href="http://localhost:91/vuhoangnhat/banglop.php" class="active">Lớp</a></li><br/>
+    <li><a href="http://localhost:91/vuhoangnhat/bangGVCN.php" class="active">GVCN</a></li><br/>
+    <li><a href="http://localhost:91/vuhoangnhat/bangkhoi.php" class="active">Khối</a></li><br/>
+    <li><a href="http://localhost:91/vuhoangnhat/bangmonhoc.php" class="active">Môn Học</a></li><br/>
+    <li><a href="http://localhost:91/vuhoangnhat/banghanhkiem.php" class="active">Hạnh Kiểm</a></li><br/>
